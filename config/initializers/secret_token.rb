bb4af9c4ffb72df7ba3da22270c13f81a7d01ef9
JevanWu <jevanwu@gmail.com>
2013-08-28 16:05:31 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VideoConference::Application.config.secret_key_base = '4a91a9acdced4850268681fb5cebffb4905d82b7aa6fe178a97bcf05a53a6057525e3b2046c0ed39dc99a95ff06e57ad1759a3ed021ed00b60e3599e5b6a6071'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VideoConference::Application.config.session_store :cookie_store, key: '_video-conference_session'
